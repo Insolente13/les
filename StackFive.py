38d167186e8407e9799d8287f4b634af36ba3e40
Insolente13 <54774329+Insolente13@users.noreply.github.com>
2019-10-20 21:39:17 +0300

    Update StackFive.py

--- StackFive.py
+++ StackFive.py
@@ -1,3 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def push(self, element):
+        self.stack.insert(self.size(), element)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop(0)
+        else:
+            return None
+
+    def peek(self):
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return None
+
+
+
 def five(brackets):
     brackets_stack = Stack()
     break_point = 0
@@ -23,3 +47,4 @@ def five(brackets):
         print('Cбалансированы')
     else:
         print('Не сбалансированы')
+
