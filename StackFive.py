d1b1895125ae6645e657a16e7af6e07626d76206
Insolente13 <54774329+Insolente13@users.noreply.github.com>
2019-10-20 21:38:45 +0300

    Create StackFive.py

---
+++ StackFive.py
@@ -0,0 +1,25 @@
+def five(brackets):
+    brackets_stack = Stack()
+    break_point = 0
+    count_pops = 0
+
+    for x in brackets:
+        brackets_stack.push(x)
+    if brackets_stack.size() % 2 == 0 and brackets_stack.peek() != '(':
+        for brack in range(brackets_stack.size()):
+            brack_pop = brackets_stack.pop()
+            if count_pops < 0:
+                break_point += 1
+                break
+            else:
+                if brack_pop == '(':
+                    count_pops += 1
+                elif brack_pop == ')':
+                    count_pops -= 1
+    else:
+        break_point += 1
+
+    if break_point == 0 and count_pops == 0:
+        print('Cбалансированы')
+    else:
+        print('Не сбалансированы')
