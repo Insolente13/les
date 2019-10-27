3bd0686c759e2f300d9691460a242d537fbc21b9
Insolente13 <54774329+Insolente13@users.noreply.github.com>
2019-10-27 17:40:59 +0300

    Update Queue_node.py

--- Queue_node.py
+++ Queue_node.py
@@ -1,76 +1,51 @@
-class Node:
-    def __init__(self, v):
-        self.value = v
-        self.next = None
-
-
-class LinkedList:
+class Stack:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.stack = []
 
-    def add_in_tail(self, item):
-        if self.head is None:
-            self.head = item
-        else:
-            self.tail.next = item
-        self.tail = item
+    def size(self):
+        return len(self.stack)
 
-    def delete(self):
-        node = self.head
-        #  удаляем первое и единственное
-        if node is not None and node.next is None:
-            self.head = None
-            self.tail = None
-        #  удаляем первое и НЕ единственное
-        elif node is not None and node.next is not None:
-            self.head = node.next
+    def push(self, element):
+        self.stack.insert(self.size(), element)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop(0)
         else:
             return None
 
-    def len(self):
-        node = self.head
-        count_node = 0
-        while node is not None:
-            count_node += 1
-            node = node.next
-        return count_node
-
-    def in_stack(self):
-        stack_list = []
-        node = self.head
-        while node is not None:
-            stack_list.append(node.value)
-            node = node.next
-        return stack_list
+    def peek(self):
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return None
 
 
 class QueueDoubleStack:
     def __init__(self):
-        self.stack_1 = LinkedList()
-        self.stack_2 = LinkedList()
+        self.stack_1 = Stack()
+        self.stack_2 = Stack()
         # инициализация хранилища данных
 
     def enqueue(self, item):
-        self.stack_1.add_in_tail(Node(item))
+        self.stack_1.push(item)
         # вставка в хвост
 
     def dequeue(self):
-        if self.stack_2.len() == 0 and self.stack_1.len() != 0:
-            self.stack_2.add_in_tail(self.stack_1.head)
-            while self.stack_1.len() != 0:
-                self.stack_1.delete()
+        if self.stack_2.size() == 0 and self.stack_1.size() != 0:
+            while self.stack_1.size() != 0:
+                self.stack_2.push(self.stack_1.pop())
 
-        del_element = self.stack_2.head
-        self.stack_2.delete()
+        del_element = self.stack_2.pop()
+
         if del_element is None:
             return None
         else:
-            return del_element.value
+            return del_element
         # выдача из головы
 
     def size(self):
-        return self.stack_1.len() + self.stack_2.len()  # размер очереди
+        return self.stack_1.size() + self.stack_2.size()  # размер очереди
 
     '''6.3. Напишите функцию, которая "вращает" очередь по кругу на N элементов.'''
     def circle(self, count):
